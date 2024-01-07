8457080c5316af907d04d3c828136f3c41b6e4a8
LuigiantonioIacomino <iacominoluigi54@gmail.com>
2024-01-07 19:41:12 +0100

    finish

--- src/main/java/org/generation/lesson/controller/SongController.java
+++ src/main/java/org/generation/lesson/controller/SongController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 
@@ -14,6 +15,7 @@ public class SongController {
 
     private ArrayList<Song> list= new ArrayList<Song>();
     private ArrayList<Song> getSongs() {
+        list= new ArrayList<Song>();
         list.add(new Song(1,"Roar","Katy Perry"));
         list.add(new Song(2,"Chandelier","Sia"));
         return list;
@@ -21,11 +23,26 @@ public class SongController {
 
     @GetMapping
     public String song (Model model) {
-    ArrayList<Song> songs=getSongs();
+        ArrayList<Song> songs=getSongs();
     model.addAttribute("lista",songs);
     return "listSongs";
-
     }
+
+    @GetMapping("/details")
+    public String detail (@RequestParam (name="id") int idSong, Model model) {
+        Song canzone=null;
+        if(idSong==1) {
+            canzone=getSongs().get(0);
+        }
+        else if(idSong==2) {
+            canzone=getSongs().get(1);
+        }
+
+        model.addAttribute("canzone",canzone);
+       return "DetailSong";
+    }
+
+
 
 
 
